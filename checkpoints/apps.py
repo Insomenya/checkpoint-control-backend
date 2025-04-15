59b40644a407c5e5a009a0c7fbbda4a8b455e913
Insomenya <kolowertnov.alexej@yandex.com>
2025-04-15 19:23:48 +0400

    added verbose app names

--- checkpoints/apps.py
+++ checkpoints/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class CheckpointsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkpoints'
+    verbose_name = _('КПП и зоны')
--- expeditions/apps.py
+++ expeditions/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ExpeditionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'expeditions'
+    verbose_name = _('Экспедиции и ТМЦ')
